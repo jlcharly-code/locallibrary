11dcd55c956aa66eb18f9d6ec4fc06a8a6472fb1
jlcharly-code <jlcharly@gmail.com>
2026-08-03 22:02:14 -0400

    language

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -21,6 +21,7 @@ except ImportError:
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LANGUAGE_CODE = 'fr-fr'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
